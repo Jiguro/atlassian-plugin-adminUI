49618dde535971236cb34bee2e252d37605bf15b
Hao Tri Pham <h.pham@celix.at>
2017-07-12 12:03:14 +0200

    Control access with SAL

--- src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
+++ src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
@@ -1,25 +1,62 @@
 package com.atlassian.plugins.tutorial.refapp;
 
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.sal.api.user.UserProfile;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
 
+@Scanned
 public class MyPluginServlet extends HttpServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(MyPluginServlet.class);
 
-    @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-            throws ServletException, IOException {
-        LOG.info("Incoming request.");
+    @ComponentImport
+    private final UserManager userManager;
 
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World!</body></html>");
+    @ComponentImport
+    private final LoginUriProvider loginUriProvider;
+
+    @Inject
+    public MyPluginServlet(final UserManager userManager, final LoginUriProvider loginUriProvider) {
+        this.userManager = userManager;
+        this.loginUriProvider = loginUriProvider;
     }
 
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        LOG.info("Incoming request.");
+
+        final UserProfile user = userManager.getRemoteUser(req);
+        if (user == null || !userManager.isSystemAdmin(user.getUserKey())) {
+            redirectToLogin(req, resp);
+            return;
+        }
+        resp.setContentType("text/html");
+        resp.getWriter().write(String.format("<html><body>Hi again, %s! Looking good ^_^</body></html>", user.getFullName()));
+    }
+
+    private void redirectToLogin(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+    }
+
+    private URI getUri(final HttpServletRequest request) {
+        final StringBuffer builder = request.getRequestURL();
+        if (StringUtils.isNotEmpty(request.getQueryString())) {
+            builder.append('?');
+            builder.append(request.getQueryString());
+        }
+        return URI.create(builder.toString());
+    }
 }
