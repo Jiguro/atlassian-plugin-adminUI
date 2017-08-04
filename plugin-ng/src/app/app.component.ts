af1e5be1a4f786404279359f28dce24d76181481
Hao Tri Pham <h.pham@celix.at>
2017-08-04 16:45:57 +0200

    Integrate Angular 3rd party module

--- plugin-ng/src/app/app.component.ts
+++ plugin-ng/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {TreeviewConfig, TreeviewItem} from 'ngx-treeview';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private title: String = 'app';
+  private treeSelectConfig: TreeviewConfig = TreeviewConfig.create({
+    hasAllCheckBox: false,
+    hasFilter: true,
+    hasCollapseExpand: false
+  });
+  private treeSelectItems = new TreeviewItem({
+    text: 'IT', value: 9, children: [
+      {
+        text: 'Programming', value: 91, children: [{
+        text: 'Frontend', value: 911, children: [
+          {text: 'Angular 1', value: 9111},
+          {text: 'Angular 2', value: 9112},
+          {text: 'ReactJS', value: 9113}
+        ]
+      }, {
+        text: 'Backend', value: 912, children: [
+          {text: 'C#', value: 9121},
+          {text: 'Java', value: 9122},
+          {text: 'Python', value: 9123, checked: false}
+        ]
+      }]
+      },
+      {
+        text: 'Networking', value: 92, children: [
+        {text: 'Internet', value: 921},
+        {text: 'Security', value: 922}
+      ]
+      }
+    ]
+  });
+
+  private treeItemOnSelect(event: UIEvent): void {
+    console.log(event.target);
+  }
 }
--- plugin-ng/src/app/app.module.ts
+++ plugin-ng/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TreeviewModule} from 'ngx-treeview';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    TreeviewModule.forRoot(),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
