a68f11395686b841eb87502c769cdaef51911d3f
Hao Tri Pham <h.pham@celix.at>
2017-08-07 18:50:06 +0200

    Adapt ngx-treeview module's behaviour

--- plugin-ng/src/app/app.component.ts
+++ plugin-ng/src/app/app.component.ts
@@ -1,6 +1,8 @@
+import * as _ from 'lodash';
 import {Component} from '@angular/core';
 
-import {TreeviewConfig, TreeviewItem} from 'ngx-treeview';
+import {TreeviewComponent, TreeviewConfig, TreeviewItem} from 'ngx-treeview';
+import {TreeviewItemComponent} from 'ngx-treeview/src/treeview-item.component';
 
 @Component({
   selector: 'app-root',
@@ -8,39 +10,157 @@ import {TreeviewConfig, TreeviewItem} from 'ngx-treeview';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private title: String = 'app';
-  private treeSelectConfig: TreeviewConfig = TreeviewConfig.create({
+  title: String = 'Demo App';
+  treeSelectConfig: TreeviewConfig = TreeviewConfig.create({
     hasAllCheckBox: false,
     hasFilter: true,
     hasCollapseExpand: false
   });
-  private treeSelectItems = new TreeviewItem({
-    text: 'IT', value: 9, children: [
-      {
-        text: 'Programming', value: 91, children: [{
-        text: 'Frontend', value: 911, children: [
-          {text: 'Angular 1', value: 9111},
-          {text: 'Angular 2', value: 9112},
-          {text: 'ReactJS', value: 9113}
-        ]
-      }, {
-        text: 'Backend', value: 912, children: [
-          {text: 'C#', value: 9121},
-          {text: 'Java', value: 9122},
-          {text: 'Python', value: 9123, checked: false}
-        ]
-      }]
-      },
-      {
-        text: 'Networking', value: 92, children: [
-        {text: 'Internet', value: 921},
-        {text: 'Security', value: 922}
-      ]
-      }
-    ]
-  });
+  treeSelectItems: TreeviewItem[] = [];
 
-  private treeItemOnSelect(event: UIEvent): void {
-    console.log(event.target);
+  constructor() {
+    // Module ngx-treeview only counts items without children as selected when checked.
+    // Override this behaviour so that both items with children or without count as selected when checked
+    TreeviewItem.prototype.getCheckedItems = function () {
+      let checkedItems = [];
+      if (this.internalChecked) {
+        checkedItems.push(this);
+      }
+
+      if (!_.isNil(this.internalChildren)) {
+        const childCount = this.internalChildren.length;
+        for (let i = 0; i < childCount; i++) {
+          checkedItems = _.concat(checkedItems, this.internalChildren[i].getCheckedItems());
+        }
+      }
+      return checkedItems;
+    };
+
+    // Module ngx-treeview automatically checks/unchecks all children when checking/unchecking a parent item.
+    // Override this behaviour so that this automatic checking/unchecking of children does not take place.
+    TreeviewItem.prototype.setCheckedRecursive = function (value) {
+    };
+
+    // Module ngx-treeview automatically checks parent when all children are checked (and unchecks parent when at least one child is unchecked).
+    // Override this behaviour so that this automatic checking/unchecking of parents does not take place
+    TreeviewItemComponent.prototype.onChildCheckedChange = function (child, checked) {
+      this.checkedChange.emit(checked);
+    };
+
+    let alreadyPatchedUpdateRefChecked = false;
+    TreeviewComponent.prototype['updateFilterItems'] = function () {
+      const _this = this;
+      if (this.filterText !== '') {
+        const filterItems_1 = [];
+        const filterText_1 = this.filterText.toLowerCase();
+        this.items.forEach(function (item) {
+          const newItem = _this.filterItem(item, filterText_1);
+          if (!_.isNil(newItem)) {
+            filterItems_1.push(newItem);
+
+            // Ensure overriden behaviour also applies during filtering
+            if (!alreadyPatchedUpdateRefChecked && newItem.constructor.name === 'FilterTreeviewItem') {
+              alreadyPatchedUpdateRefChecked = true;
+              newItem.constructor.prototype['updateRefChecked'] = function () {
+                this.children.forEach(function (child) {
+                  if (child.constructor.name === 'FilterTreeviewItem') {
+                    child.updateRefChecked();
+                  }
+                });
+                this.refItem.checked = this.checked;
+              };
+            }
+          }
+        });
+        this.filterItems = filterItems_1;
+      } else {
+        this.filterItems = this.items;
+      }
+      this.updateCheckedAll();
+    };
+
+    // Module ngx-treeview displays "All" selected wrongly, when top level items are selected during filtering
+    // Override this behaviour so that "All" is only displayed when all items on all hierarchies are selected
+    TreeviewComponent.prototype.onItemCheckedChange = function (item, checked) {
+      if (this.allItem.checked !== checked) {
+        let allItemChecked = true;
+        for (let i = 0; i < this.items.length; i++) {
+          if (!isCheckedRecursive(this.items[i])) {
+            allItemChecked = false;
+            break;
+          }
+        }
+        if (this.allItem.checked !== allItemChecked) {
+          this.allItem.checked = allItemChecked;
+        }
+      }
+      if (item.constructor.name === 'FilterTreeviewItem') {
+        item['updateRefChecked']();
+      }
+      this.raiseSelectedChange();
+    };
+    TreeviewComponent.prototype['updateCheckedAll'] = function () {
+      let hasItemUnchecked = false;
+      for (let i = 0; i < this.items.length; i++) {
+        if (!isCheckedRecursive(this.items[i])) {
+          hasItemUnchecked = true;
+          break;
+        }
+      }
+      if (this.allItem.checked === hasItemUnchecked) {
+        this.allItem.checked = !hasItemUnchecked;
+      }
+    };
+
+    function isCheckedRecursive(item: TreeviewItem): boolean {
+      if (!item.checked) {
+        return false;
+      }
+
+      if (!_.isNil(item.children)) {
+        for (let i = 0; i < item.children.length; i++) {
+          if (!isCheckedRecursive(item.children[i])) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    this.treeSelectItems.push(new TreeviewItem({
+      text: 'Austria', value: '1', checked: false, children: [{
+        text: 'Styria', value: '1-1', checked: false, children: [{
+          text: 'Graz', value: '1-1-1', checked: false
+        }, {
+          text: 'Mariazell', value: '1-1-2', checked: false
+        }]
+      }, {
+        text: 'Tyrol', value: '1-2', checked: false, children: [{
+          text: 'Oberndorf', value: '1-2-1', checked: false
+        }]
+      }]
+    }));
+    this.treeSelectItems.push(new TreeviewItem({
+      text: 'United Kingdom', value: '2', checked: false, children: [{
+        text: 'Cambridge', value: '2-1', checked: false
+      }, {
+        text: 'Edinburgh', value: '2-2', checked: false
+      }, {
+        text: 'Manchester', value: '2-3', checked: false
+      }]
+    }));
+    this.treeSelectItems.push(new TreeviewItem({
+      text: 'Hong Kong', value: '3', checked: false
+    }));
+    this.treeSelectItems.push(new TreeviewItem({
+      text: 'Japan', value: '4', checked: false, children: [{
+        text: 'Tokyo', value: '4-1', checked: false
+      }]
+    }));
+  }
+
+  treeItemOnSelect(event: UIEvent): void {
+    console.log(event);
   }
 }
