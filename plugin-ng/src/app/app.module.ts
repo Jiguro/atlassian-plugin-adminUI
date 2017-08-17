62e0117a7486a5c440373f781cd223a909b2e03a
Hao Tri Pham <phamhaotri@googlemail.com>
2017-08-17 17:16:58 +0100

    Allow bootstrapping of multiple root components

--- plugin-ng/src/app/app.module.ts
+++ plugin-ng/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {ApplicationRef, ComponentFactoryResolver, Inject, NgModule, OpaqueToken, Type} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {TreeviewI18n, TreeviewModule} from 'ngx-treeview';
 import {TreeviewI18nImpl} from './TreeViewI18nImpl';
+
+export const BOOTSTRAP_COMPONENTS_TOKEN = new OpaqueToken('bootstrap_components');
 
 @NgModule({
   declarations: [
@@ -14,7 +16,17 @@ import {TreeviewI18nImpl} from './TreeViewI18nImpl';
     BrowserModule
   ],
   providers: [{provide: TreeviewI18n, useClass: TreeviewI18nImpl}],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
 export class AppModule {
+  constructor(private resolver: ComponentFactoryResolver,
+              @Inject(BOOTSTRAP_COMPONENTS_TOKEN) private components) {}
+
+  ngDoBootstrap(appRef: ApplicationRef) {
+    this.components.forEach((componentDef: {type: Type<any>, selector: string}) => {
+      const factory = this.resolver.resolveComponentFactory(componentDef.type);
+      factory['factory'].selector = componentDef.selector;
+      appRef.bootstrap(factory);
+    });
+  }
 }
--- plugin-ng/src/main.ts
+++ plugin-ng/src/main.ts
@@ -1,11 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule, BOOTSTRAP_COMPONENTS_TOKEN } from './app/app.module';
 import { environment } from './environments/environment';
+import {AppComponent} from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const components = [
+  { type: AppComponent, selector: 'app-root#no1' },
+  { type: AppComponent, selector: 'app-root#no2' }
+];
+
+const platform = platformBrowserDynamic([
+  {provide: BOOTSTRAP_COMPONENTS_TOKEN, useValue: components}
+]);
+platform.bootstrapModule(AppModule);
